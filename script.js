1804c39b37d5e88499bd881b2d9d392868185501
Ajay-sys2-tech <ajaycu23@gmail.com>
2022-02-08 21:46:47 +0530

    added negative number validation

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ function compute()
     
     if(p == ""){
         alert("Principal amount can not be left blank");
+        document.getElementById("principal").focus();
+    }
+    else if(parseInt(p) <=0){
+        alert("Please enter a positive number");
+        document.getElementById("principal").focus();
     }
     var interest = (p*r*t)/100;
 
     const year = parseInt(2022) + parseInt(t);
 
-    let string = `If you deposit ${p}, <br>
-     at an interest rate of ${r}%.<br>
-     You will receive an amount of ${interest},<br>
-     in the year ${year}<br>
+    let string = `If you deposit <mark>${p}</mark>, <br>
+     at an interest rate of <mark>${r}%</mark>.<br>
+     You will receive an amount of <mark>${interest}</mark>,<br>
+     in the year <mark>${year}<mark><br>
     `;
 
-    if(p != ""){
+    if(0 < parseInt(p)){
         document.getElementById("result").style.display= "block";
         document.getElementById("container").style.height= "400px";
         document.getElementById("result").innerHTML = string;
